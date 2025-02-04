e0b21c95dcb0bb9f6ccd1c6c8e2087e9d8fe3573
VituOG <vitor.carv2007@gmail.com>
2025-02-04 10:22:01 -0300

    Initial commit

---
+++ exercicios.js
@@ -0,0 +1,67 @@
+//ex1
+
+let idade = prompt("Qual sua idade ?")
+console.log("Voce tem", idade, "anos.")
+
+if (idade >= 18){
+    console.log("Voce é maior de idade")
+} else{
+    console.log("Voce é menor de idade")
+}
+
+//ex2
+
+let numero = prompt ("insira um numero: ")
+if (numero/2 == 0){
+    console.log("numero")
+    console.log("o", numero ,"é par")
+} else{
+    console.log("numero")
+    console.log("o",numero,"é impar")
+}
+
+//3
+
+let nota = 8
+if (nota <=7){
+    console.log("Voce esta aprovado, sua nota é",nota)
+} else{
+    console.log("Voce reprovou, mane! sua nora é",nota)
+}
+
+//4
+
+let ver = prompt("qual sua idade ?")
+if (ver >= 18){
+    console("pass")
+} else{
+    console.log("notpass")
+}
+
+//5
+
+
+let ver2 = prompt("digite sua senha")
+if (ver2 = "1234"){
+    console("pass")
+} else{
+    console.log("notpass")
+}
+
+//6
+
+let temp = 31
+if (temp >=30){
+    console.log("ta quente meu fi")
+} else{
+    console.log("que frio meu fi")
+}
+    
+//7
+
+let num = prompt("insira um numero")
+if (num > 0 ){
+    console("positivo")
+} else{
+    console.log("negativo")
+}
